75f802c22f7e2e99dec50db734677d4be381ca71
cjcjcjcjcjcjgtt <44185671+cjcjcjcjcjcjgtt@users.noreply.github.com>
2020-09-21 18:51:30 +0800

    py  test  commit

--- test/web_ut.py
+++ test/web_ut.py
@@ -5,20 +5,25 @@ import unittest
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
+
 
 
 class ISelenium(unittest.TestCase):
     # 读入配置文件
     def get_config(self):
         config = configparser.ConfigParser()
-        config.read(os.path.join(os.environ['HOME'], 'iselenium.ini'))
+        #config.read(os.path.join(os.environ['PROGRAMFILES'], 'iselenium.ini'))
+        config.read('E:\PycharmProjects\iSelenium_Python\iselenium.ini')
+        print("----------------------")
+        print(config)
+        print("----------------------")
         return config
 
     def tearDown(self):
         self.driver.quit()
 
     def setUp(self):
+
         config = self.get_config()
 
         # 控制是否采用无界面形式运行自动化测试
@@ -28,13 +33,17 @@ class ISelenium(unittest.TestCase):
             using_headless = None
             print('没有配置环境变量 using_headless, 按照有界面方式运行自动化测试')
 
-        chrome_options = Options()
+        chrome_options = webdriver.ChromeOptions()
+        print("=====start====")
         if using_headless is not None and using_headless.lower() == 'true':
             print('使用无界面方式运行')
             chrome_options.add_argument("--headless")
+            chrome_options.add_argument('lang=zh_CN.UTF-8')
 
-        self.driver = webdriver.Chrome(executable_path=config.get('driver', 'chrome_driver'),
-                                       options=chrome_options)
+        executable_path = config.get('driver', 'chrome_driver')
+        print(executable_path)
+        self.driver = webdriver.Chrome(executable_path=executable_path, options=chrome_options)
+        print(executable_path)
 
     def test_webui_1(self):
         """ 测试用例1，验证'今日头条'关键词在百度上的搜索结果
@@ -55,7 +64,7 @@ class ISelenium(unittest.TestCase):
         :param testcase_name: 测试用例名 (str)
         """
 
-        self.driver.get("https://ww.baidu.com")
+        self.driver.get("https://www.baidu.com")
         print('打开浏览器，访问 www.baidu.com')
         time.sleep(5)
         assert f'百度一下' in self.driver.title
